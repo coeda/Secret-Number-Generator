d1966994ab2f23cadb0a8c22e2c7ad79b3cc71a6
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-07 18:36:21 -1000

    Added Secret number function that returns random number between 0 and 1000000

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,11 @@
 'use strict';
 module.exports = function() {
-  
+var secretNumber;
+  return function secretNumber(){
+    var randomNumber = Math.floor((Math.random() * 1000000) + 0);
+    var secretNumber = Math.floor((Math.random() * 1000000) + 0);
+    return randomNumber;
+  };
+
+
 };
