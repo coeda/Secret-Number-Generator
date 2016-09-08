c7f5617db77fc68cdc274b3b2ac399d0d7693531
Casey Oeda <caseyoeda@Caseys-MacBook-Pro.local>
2016-09-07 18:53:40 -1000

    created secret number function, returns number generated in the exports function, returns same number in secretNumber function.

--- secret_number.js
+++ secret_number.js
@@ -1,10 +1,10 @@
 'use strict';
 module.exports = function() {
-var secretNumber;
+var sNumber = Math.floor((Math.random() * 1000000) + 0);
+
   return function secretNumber(){
-    var randomNumber = Math.floor((Math.random() * 1000000) + 0);
-    var secretNumber = Math.floor((Math.random() * 1000000) + 0);
-    return randomNumber;
+
+    return sNumber;
   };
 
 
